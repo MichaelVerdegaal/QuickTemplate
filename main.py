3ee8fa69cc997467205726e9c0cd73c5e5f27d45
Michael Verdegaal <michaelverdegaal@hotmail.nl>
2024-09-15 18:52:23 +0200

    feat: repeat file

--- main.py
+++ main.py
@@ -24,8 +24,6 @@
                 3.2.1 If transaction Name/Description in repeat.txt, give it that class
                 3.2.2 If transaction has no class, classify it with suggestions from categories.txt. Ask if this is single-time only or repeat. If repeat put it  Name/Description in repeat.txt, and class all similar transactions. If single-time, only classify this transaction.
 4. Save updated transactions to file again.
-
-
 """
 
 import pandas as pd
@@ -33,9 +31,12 @@ from config import PROJECT_FOLDER, OUTPUT_FOLDER
 from main.load import ing_loader
 
 # from transformers import pipeline
-
+"""
+Loading
+________________________________________________________________________
+"""
 # Load and preprocess data
-df = ing_loader(
+transaction_df = ing_loader(
     PROJECT_FOLDER / "data" / "NL38INGB0001546874_01-01-2024_18-08-2024.csv"
 )
 
@@ -46,15 +47,6 @@ df = ing_loader(
 #     device=0,
 # )
 
-# Read budget categories from categories.txt
-with open(PROJECT_FOLDER / "categories.txt", "r") as f:
-    budget_categories = [line.strip() for line in f.readlines()]
-
-
-"""
-________________________________________________________________________
-"""
-
 transaction_df_path = OUTPUT_FOLDER / "transactions.csv"
 # Check if the transaction file exists
 if transaction_df_path.exists():
@@ -63,7 +55,7 @@ if transaction_df_path.exists():
     existing_df["date"] = pd.to_datetime(existing_df["date"])
 
     # Append new transactions
-    combined_df = pd.concat([existing_df, df], ignore_index=True)
+    combined_df = pd.concat([existing_df, transaction_df], ignore_index=True)
 
     # Remove duplicates based on all columns
     combined_df.drop_duplicates(
@@ -75,5 +67,29 @@ if transaction_df_path.exists():
     print(f"Updated transactions saved to {transaction_df_path}")
 else:
     # If the file doesn't exist, save the current DataFrame
-    df.to_csv(transaction_df_path, index=False)
+    transaction_df["class"] = None
+    transaction_df.to_csv(transaction_df_path, index=False)
     print(f"New transactions saved to {transaction_df_path}")
+
+"""
+Classifying
+________________________________________________________________________
+"""
+
+# Read budget categories from categories.txt
+with open(PROJECT_FOLDER / "categories.txt", "r") as f:
+    budget_categories = [line.strip() for line in f.readlines()]
+
+# Get repeat account list
+repeat_df_path = OUTPUT_FOLDER / "repeat.csv"
+# Check if repeat.csv exists, if not create it, otherwise read it as a DataFrame
+if not repeat_df_path.exists():
+    # Create an empty DataFrame with the necessary columns
+    repeat_df = pd.DataFrame(columns=["account_name", "category"])
+    # Save the empty DataFrame to CSV
+    repeat_df.to_csv(repeat_df_path, index=False)
+    print(f"Created new repeat.csv at {repeat_df_path}")
+else:
+    # Read existing repeat.csv
+    repeat_df = pd.read_csv(repeat_df_path)
+    print(f"Loaded existing repeat.csv from {repeat_df_path}")
